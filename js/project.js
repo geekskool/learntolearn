ce63bed39f30609c1705b36bfbf3ada3100fafd0
rahulr91 <rayaprolurahul1991@gmail.com>
2016-05-19 12:14:37 +0530

    Made changes to line function and populated project.js to draw GeekSkool logo

--- js/project.js
+++ js/project.js
@@ -1,5 +1,24 @@
 function draw(){
-rectangle(100,100, 200, 150)
-fill('red')
-rectangle(300,400, 200, 150)
+	ctx.strokeStyle= "orange"
+	for (var i = 0; i <= 100; i++){
+		line(350+i,100,100+i,350)
+	}
+
+	for (var i = 0; i <= 100; i++) {
+		line(100+i,350,350+i,600)
+	}
+
+	for (var i = 0; i <= 100; i++) {
+		line(650+i,600,900+i,350)
+	}
+
+	for (var i = 0; i <= 100; i++){
+		line(900+i,350,650+i,100)
+	}
+
+	text("G",340,520,530);
+	fill("orange")
+	
+	
+
 }
